94360810321a73bc1f4a9a62231704714c4e68fb
Will Weaver <monkey@buildingbananas.com>
2017-12-03 12:47:25 -0500

    Migrate to todoist v8 API

--- src/todoist.js
+++ src/todoist.js
@@ -1,22 +1,22 @@
+/*
+  Based off the API defined at:
+  https://developer.todoist.com/rest/v8/#authorization
+*/
+
 'use strict'
 
 const axios = require('axios')
-
-const apiUrl = 'https://todoist.com/api/v7'
-const options = {
-  sync_token: '*',
-}
+const apiUrl = 'https://beta.todoist.com/API/v8'
 
 module.exports = {
   getProjects: (token) => {
-    const uri = `${apiUrl}/sync`
-    options.token = token
-    options.resource_types = '["projects"]'
+    const uri = `${apiUrl}/projects`
+    const options = {token}
 
     return new Promise((resolve, reject) => axios.get(uri, {params: options})
       .then(response => {
         // const sync_token = response.data.sync_token
-        resolve(response.data.projects)
+        resolve(response.data)
       })
       .catch(error => {
         reject(error.response)
